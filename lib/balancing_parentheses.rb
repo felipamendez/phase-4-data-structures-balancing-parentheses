8d0bdf507ab8b7965ad8da3ca13d8c69bf560018
Felipa Mendez <felipalouise@gmail.com>
2022-05-06 08:25:03 -0500

    commit tree e34f1cce800e213c24f3e1c52ebd5ea926862afd

--- lib/balancing_parentheses.rb
+++ lib/balancing_parentheses.rb
@@ -1,3 +1,20 @@
 require_relative './stack'
 
 # your code here
+
+def balancing_parentheses(string)
+    
+    stack = Stack.new()
+    
+    string.chars.each do |paren|
+        if stack.peek == "(" && paren == ")"
+            stack.pop()
+        else
+            stack.push(paren)
+        end
+    end
+
+    return stack.size
+
+
+end
